b7d3e9210187887d7d2356b76feee14fa99a8a8b
tudodang75@gmail.com <tudodang75@gmail.com>
2024-06-16 22:49:02 +0700

    task_3 - demtu

--- task_3 - demtu(2).py
+++ task_3 - demtu(2).py
@@ -1,9 +1,10 @@
-file_path = 'D:\GITHUB\AIO-187_homework-week2\P1_data.txt' 
+file_path = 'D:\\GITHUB\\AIO-187_homework-week2\\P1_data.txt' 
+
+counter = {}
 
 with open(file_path, 'r') as file:
-    for lines in file: 
-        words = lines.split()
-        counter = {}
+    for line in file: 
+        words = line.split()
         for word in words: 
             if word in counter: 
                 counter[word] += 1 
